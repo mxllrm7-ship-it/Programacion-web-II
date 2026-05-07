d1e8c92b0902d19376b1a846c88c00a9492701dd
papen12 <papenfabri2.0@gmail.com>
2026-05-07 06:16:02 -0400

    ,

---
+++ PHPTeresa/laravel12Teresa/resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel 12 Web II</title>
+
+    <style>
+        *{
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+        }
+
+        body{
+            height:100vh;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            background: linear-gradient(135deg, #0f172a, #312e81, #581c87);
+            font-family: Arial, Helvetica, sans-serif;
+            color:white;
+        }
+
+        .contenedor{
+            text-align:center;
+            padding:40px;
+        }
+
+        h1{
+            font-size:60px;
+            margin-bottom:20px;
+            text-shadow:0 0 15px rgba(255,255,255,0.3);
+        }
+
+        h2{
+            font-size:35px;
+            font-weight:normal;
+            color:#d1d5db;
+            margin-bottom:40px;
+        }
+
+        button{
+            padding:15px 40px;
+            border:none;
+            border-radius:12px;
+            background:#ec4899;
+            color:white;
+            font-size:18px;
+            cursor:pointer;
+            transition:0.3s;
+        }
+
+        button:hover{
+            background:#be185d;
+            transform:scale(1.05);
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="contenedor">
+
+        <h1>Proyecto Laravel 12 para Web II</h1>
+
+        <h2>Teresa Ramos Mollo</h2>
+
+        <button>Bienvenida</button>
+
+    </div>
+
+</body>
+</html>
